eefd2510f4448237c1af995c7e3fc1735f39ff02
aidant11 <aidant10@nycstudents.net>
2025-03-26 15:48:11 +0000

    123

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
-
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        WordMatch w = new WordMatch("mississippi");
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+        WordMatch game = new WordMatch("aaaabb");
+        System.out.println(game.scoreGuess("a"));
+        System.out.println(game.scoreGuess("aa"));
+        System.out.println(game.scoreGuess("aaa"));
+        System.out.println(game.scoreGuess("aabb"));
+        System.out.println(game.scoreGuess("c"));
+        WordMatch game2 = new WordMatch("concentration");
+        System.out.println(game2.scoreGuess("ten"));
+    }
+}
