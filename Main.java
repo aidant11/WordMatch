c97f16dbf1f53beecddfbfb664d888f37549eec2
aidant11 <aidant10@nycstudents.net>
2025-05-05 21:46:52 +0000

    123

--- Main.java
+++ Main.java
@@ -1,24 +1,24 @@
-public class Main 
+public class Main
 {
     public static void main(String[] args)
     {
         WordMatch w = new WordMatch("mississippi");
         System.out.println(w.scoreGuess("i"));
         System.out.println(w.scoreGuess("iss"));
-        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("issippi"));
         System.out.println(w.scoreGuess("mississippi"));
-        WordMatch game = new WordMatch("aaaabb");
-        System.out.println(game.scoreGuess("a"));
-        System.out.println(game.scoreGuess("aa"));
-        System.out.println(game.scoreGuess("aaa"));
-        System.out.println(game.scoreGuess("aabb"));
-        System.out.println(game.scoreGuess("c"));
-        WordMatch game = new WordMatch("concentration");
+        WordMatch game = new WordMatch("concatenation");
         System.out.println(game.scoreGuess("ten"));
         System.out.println(game.scoreGuess("nation"));
         System.out.println(game.findBetterGuess("ten", "nation"));
         System.out.println(game.scoreGuess("con"));
         System.out.println(game.scoreGuess("cat"));
         System.out.println(game.findBetterGuess("con", "cat"));
+        WordMatch game2 = new WordMatch("cataraction");
+        System.out.println(game2.scoreGuess("ca"));
+        System.out.println(game2.scoreGuess("cat"));
+        System.out.println(game2.scoreGuess("cataract"));
+        System.out.println(game2.scoreGuess("cataracts"));
+        System.out.println(game2.scoreGuess("cataractsurgery"));
     }
 }
--- WordMatch.java
+++ WordMatch.java
@@ -1,45 +1,35 @@
 public class WordMatch {
-    /** The secret string. */
     private String secret;
 
-
-    /**
-     * Constructs a WordMatch object with the given secret string of lowercase
-     * letters.
-     */
     public WordMatch(String word) {
         secret = word;
     }
 
-
-    /**
-     * Returns a score for guess, as described in part (a).
-     * Precondition: 0 < guess.length() <= secret.length()
-     */
     public int scoreGuess(String guess) {
-        int score = 0;
-        for(int i = 0; i<= secret.length() - guess.length(); i++)
-            {
-                if (secret.substring(i, i + guess.length()).equals(guess))
-                {
-                    score++;
-                }
+        int count = 0;
+        int guessLength = guess.length();
+        int limit = secret.length() - guessLength;
+
+        for (int i = 0; i <= limit; i++) {
+            String sub = secret.substring(i, i + guessLength);
+            if (sub.equals(guess)) {
+                count++;
             }
-            return score * guess.length() * guess.length();
+        }
+
+        return count * guessLength * guessLength;
     }
 
+    public String findBetterGuess(String guess1, String guess2) {
+        int score1 = scoreGuess(guess1);
+        int score2 = scoreGuess(guess2);
 
-    /**
-     * Returns the better of two guesses, as determined by scoreGuess and the rules
-     * for a
-     * tie-breaker that are described in part (b).
-     * Precondition: guess1 and guess2 contain all lowercase letters.
-     * guess1 is not the same as guess2.
-     */
-    public String findBetterGuess(String guess1, String guess2) 
-    {
-        if (scoreGuess(guess1) > scoreGuess(guess2)) return guess1;
-        if (guess1.compareTo(guess2) > 0) return guess1;
-        return guess2;
+        if (score1 > score2) {
+            return guess1;
+        } else if (score2 > score1) {
+            return guess2;
+        } else {
+            return (guess1.compareTo(guess2) > 0) ? guess1 : guess2;
+        }
     }
 }
